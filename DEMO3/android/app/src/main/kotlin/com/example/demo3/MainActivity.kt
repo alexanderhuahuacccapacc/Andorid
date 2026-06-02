eb88d2f391309db299d2dae261e918e74e338928
alexanderhuahuacccapacc <142243666+alexanderhuahuacccapacc@users.noreply.github.com>
2026-06-02 14:42:27 -0500

    intento02

--- DEMO3/android/app/src/main/kotlin/com/example/demo3/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.demo3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lista_tareas/android/app/src/main/kotlin/com/example/lista_tareas/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lista_tareas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
